939357d5e6082d75b2337bae94270be20091b082
Denis Valeev <valeevden@gmail.com>
2020-11-06 04:13:26 +0300

    add Complex 2Dimensional Array tests

--- LibraryForMethods.Tests/Array2DimensionalTest.cs
+++ LibraryForMethods.Tests/Array2DimensionalTest.cs
@@ -5,16 +5,16 @@ using System.Text;
 
 namespace LibraryForMethods.Tests
 {
-     public class Array2DimensionalTest
-     {
-        [TestCase(1,1)]
-        [TestCase(2,-15)]
-        [TestCase(3,-34)]
-        [TestCase(4,0)]
-        public void FindMin2DArrayUnit(int MockNumber, int expected)
+    public class Array2DimensionalTest
+    {
+        [TestCase(1, 1)]
+        [TestCase(2, -15)]
+        [TestCase(3, -34)]
+        [TestCase(4, 0)]
+        public void FindMin2DArrayUnitTest(int MockNumber, int expected)
         {
-            int actual = LibraryForMethods.Array2Dimensional.FindMin2DArrayUnit(GetArrayMock(MockNumber));
-            
+            int actual = Array2Dimensional.FindMin2DArrayUnit(GetArrayMock(MockNumber));
+
             Assert.AreEqual(expected, actual);
         }
 
@@ -22,13 +22,35 @@ namespace LibraryForMethods.Tests
         [TestCase(2, 13)]
         [TestCase(3, 78)]
         [TestCase(4, 0)]
-        public void FindMax2DArrayUnit(int MockNumber, int expected)
+        public void FindMax2DArrayUnitTest(int MockNumber, int expected)
         {
-            int actual = LibraryForMethods.Array2Dimensional.FindMax2DArrayUnit(GetArrayMock(MockNumber));
+            int actual = Array2Dimensional.FindMax2DArrayUnit(GetArrayMock(MockNumber));
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase(1, new int[] { 2, 2 })]
+        [TestCase(2, new int[] { 1, 0 })]
+        [TestCase(3, new int[] { 1, 3 })]
+        [TestCase(4, new int[] { 0, 0 })]
+        public void FindMax2DArrayUnitIndexTest(int MockNumber, int[] expected)
+        {
+            int[] actual = Array2Dimensional.FindMax2DArrayUnitIndex(GetArrayMock(MockNumber));
 
             Assert.AreEqual(expected, actual);
         }
 
+        [TestCase(1, 1)]
+        [TestCase(3, 3)]
+        [TestCase(4, 4)]
+        public void MakeNewTransposedArrayTest(int GetActualArrayMockNumber, int GetExpectedArrayMockNumber)
+        {
+            int[,] actual = Array2Dimensional.MakeNewTransposedArray(GetArrayMock(GetActualArrayMockNumber));
+            int[,] expected = GetExpMock(GetExpectedArrayMockNumber);
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        // Здесь создается  Мок двумерного массива на вход теста
         int[,] GetArrayMock(int n)
         {
             switch (n)
@@ -63,7 +85,36 @@ namespace LibraryForMethods.Tests
                 default:
                     throw new Exception();
             }
-            
+        }
+        // А здесь Мок двумерного массива на выход дял транспорирования
+        int[,] GetExpMock(int n)
+        {
+            switch (n)
+            {
+                case 1:
+                    return new int[,]
+                    {
+                        {1,4,7},
+                        {2,5,8},
+                        {3,6,9}
+                    };
+                case 3:
+                    return new int[,]
+                    {
+                        {0,-6,33},
+                        {12,34,22},
+                        {6,8,12},
+                        {-34,78,-3}
+                    };
+                case 4:
+                    return new int[,]
+                    {
+                        {0,0},
+                        {0,0},
+                    };
+                default:
+                    throw new Exception();
+            }
 
         }
     }
